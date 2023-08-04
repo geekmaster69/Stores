de77d0219beb66c396b8c01d1fbd59f3ef2c6199
Geeckmaster69 <emmanuelmosa@gmail.com>
2023-08-03 18:52:31 -0600

    First Commit

--- app/src/main/java/com/example/stores/MainActivity.kt
+++ app/src/main/java/com/example/stores/MainActivity.kt
@@ -1,14 +1,58 @@
 package com.example.stores
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.GridLayoutManager
 import com.example.stores.databinding.ActivityMainBinding
+import java.util.concurrent.LinkedBlockingQueue
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnClickListener {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var mAdapter: StoreAdapter
+    private lateinit var mGridLayout: GridLayoutManager
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        //Codigo Temporal
+        binding.btnSave.setOnClickListener {
+            val store = StoreEntity(name = binding.etName.text.toString().trim())
+
+            Thread {
+                StoreApplication.database.storeDao().addStore(store)
+            }.start()
+
+
+            mAdapter.add(store)
+        }
+
+        setupRecyclerview()
+    }
+
+    private fun setupRecyclerview() {
+        mAdapter = StoreAdapter(mutableListOf(), this)
+        mGridLayout = GridLayoutManager(this, 2)
+        getStores()
+
+        binding.recyclerView.apply {
+            setHasFixedSize(true)
+            layoutManager = mGridLayout
+            adapter = mAdapter
+        }
+    }
+
+    private fun getStores() {
+        val queue = LinkedBlockingQueue<MutableList<StoreEntity>>()
+        Thread {
+            val stores = StoreApplication.database.storeDao().getAllStores()
+            queue.add(stores)
+        }.start()
+
+        mAdapter.setStores(queue.take())
+    }
+
+    override fun onClick(storeEntity: StoreEntity) {
+        TODO("Not yet implemented")
     }
 }
---
+++ app/src/main/java/com/example/stores/StoreAdapter.kt
@@ -0,0 +1,52 @@
+package com.example.stores
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.stores.databinding.ItemStoreBinding
+
+class StoreAdapter(private var stores: MutableList<StoreEntity>, private var listener: OnClickListener) :
+    RecyclerView.Adapter<StoreAdapter.ViewHolder>() {
+    private lateinit var mContext: Context
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        mContext = parent.context
+        val view = LayoutInflater.from(mContext).inflate(R.layout.item_store, parent, false)
+
+        return ViewHolder(view)
+
+    }
+
+    override fun getItemCount(): Int = stores.size
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val store = stores[position]
+        with(holder) {
+            setListener(store)
+            binding.tvName.text = store.name
+        }
+    }
+
+    fun add(storeEntity: StoreEntity) {
+        stores.add(storeEntity)
+        notifyDataSetChanged()
+    }
+
+    fun setStores(stores: MutableList<StoreEntity>) {
+        this.stores = stores
+        notifyDataSetChanged()
+    }
+
+    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val binding = ItemStoreBinding.bind(view)
+
+        fun setListener(storeEntity: StoreEntity) {
+            binding.root.setOnClickListener {
+                listener.onClick(storeEntity)
+            }
+        }
+    }
+}
